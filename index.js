ca8e13a4c200c85aaff6d5545acf7ee6da5b8a29
Abhijith Babu <abijithb2002@gmail.com>
2025-04-17 22:40:26 +0530

    chore: cors origins updated

--- index.js
+++ index.js
@@ -24,10 +24,22 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  process.env.SERVER_URL,
+  process.env.CLIENT_URL_PROD,
+  process.env.SERVER_URL_PROD,
+];
 // Configure CORS with specific options
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
